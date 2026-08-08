64691bfa9d0d63acc911dadccf9a10dc9f9abdd6
METHEW <yotoqxona.com@gmail.com>
2026-08-08 13:44:00 +0900

    feat: built project standarts

--- apps/nestar-api/src/app.module.ts
+++ apps/nestar-api/src/app.module.ts
@@ -5,6 +5,8 @@ import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from "@nestjs/graphql";
 import { ApolloDriver } from "@nestjs/apollo";
 import { AppResolver } from './app.resolver';
+import { ComponentsModule } from './components/components.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [ConfigModule.forRoot(),
@@ -14,6 +16,8 @@ import { AppResolver } from './app.resolver';
     uploads: false,
     autoSchemaFile: true,
   }),
+  ComponentsModule,
+  DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService, AppResolver],
