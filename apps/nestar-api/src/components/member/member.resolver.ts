9d89b608281e803835c89fbde6261bf2260020a1
METHEW <yotoqxona.com@gmail.com>
2026-08-13 18:15:39 +0900

    feat validation pipe

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -1,5 +1,7 @@
-import { Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { MemberService } from './member.service';
+import { UsePipes, ValidationPipe } from '@nestjs/common';
+import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
 
 
 @Resolver()
@@ -7,15 +9,21 @@ export class MemberResolver {
     constructor(private readonly memberService: MemberService) { }
 
     @Mutation(() => String)
-    public async signup(): Promise<string> {
-        console.log("mutation: signup")
-        return this.memberService.signup();
+    @UsePipes(ValidationPipe)
+    public async signup(
+        @Args("input") input: MemberInput
+    ): Promise<string> {
+        console.log("mutation: signup");
+        console.log("input:", input);
+
+        return this.memberService.signup(input);
     }
 
     @Mutation(() => String)
-    public async login(): Promise<string> {
+    @UsePipes(ValidationPipe)
+    public async login(@Args("input") input: LoginInput): Promise<string> {
         console.log("mutation: login")
-        return this.memberService.login();
+        return this.memberService.login(input);
     }
 
     @Mutation(() => String)
--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -1,18 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { MemberModule } from './member.module';
 import { Model } from 'mongoose';
+import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
 
 @Injectable()
 export class MemberService {
 
     constructor(@InjectModel("Member") private readonly memberModel: Model<null>) { }
 
-    public async signup(): Promise<string> {
+    public async signup(input: MemberInput): Promise<string> {
         return "signup executed";
     }
 
-    public async login(): Promise<string> {
+    public async login(input: LoginInput): Promise<string> {
         return "login executed";
     }
 
