7aede80543114b687c82ed6bacb3e6f0be10d8f2
METHEW <yotoqxona.com@gmail.com>
2026-09-09 16:52:09 +0900

    fix : modify batchModel and commect mongodb

--- apps/nestar-batch/src/batch.service.ts
+++ apps/nestar-batch/src/batch.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class NestarBatchService {
+export class BatchService {
   getHello(): string {
-    return 'Welcome to Nestar Batch Server!';
+    return 'Welcome to Batch Server!';
   }
 }
